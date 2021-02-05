b6c88105da70b9cf2bc3af2838db8059f0bc62c3
Francisco Gomez Aguilera <fr4231gm@gmail.com>
2021-02-05 17:44:46 +0100

    Modificada la transimison y la recepción de datos

--- receiver/Emivosr_v3/Emivosr_v3.ino
+++ receiver/Emivosr_v3/Emivosr_v3.ino
@@ -7,21 +7,87 @@
 * Library: TMRh20/RF24, https://github.com/tmrh20/RF24/
 */
 #include <SPI.h>
-#include <nRF24L01.h>
 #include <RF24.h>
 RF24 radio(9, 10); // CE, CSN
 const byte address[6] = "00001";
 
+//Valores para los joysticks y declaración de variables
+
+int acelerador = A0;
+int girador = A1;
+int valorAcelerador = 0;
+int valorGirador = 0;
+
+
+
+struct package
+{
+  float valorAceleracionDelante  = 0.0;
+  float valorAceleracionDetras = 0.0;
+  int  direccion = 0;
+};
+typedef struct package Package;
+Package data;
+
+
+
 
 void setup() {
+  Serial.begin(9600);
   radio.begin();
+  delay(1000);
+  Serial.println("Inicializando sistema de transmisión");
   radio.openWritingPipe(address);
   radio.setPALevel(RF24_PA_MIN);
   radio.stopListening();
+  Serial.println("Comunication address: 00001");
+  delay(400);
+  Serial.println("Power Leveol: " + RF24_PA_MIN);
+  delay(400);
+  Serial.println("Bienvendio");
+  delay(200);
+  Serial.println("Sistema inicializado");
+  
 }
 void loop() {
-  float data[3];
-  data[0] = 1.2;
+
+
+  valorAcelerador = analogRead(acelerador);
+  valorGirador = analogRead(girador);
+
+
+  if(valorGirador <462 || valorGirador > 562){
+    if(valorGirador <462){
+    data.direccion = 4;
+    data.valorAceleracionDelante = 0.0;
+    data.valorAceleracionDetras = 0.0;
+  }
+  
+  if(valorGirador > 562){
+    data.direccion = 2;
+    data.valorAceleracionDelante = 0.0;
+    data.valorAceleracionDetras = 0.0;
+  }
+  }else{
+    data.direccion = 1;
+    Serial.print("Valor acelerador");
+    Serial.println(valorAcelerador);
+    if(valorAcelerador < 462){
+    //Acelerador en modo marcha atrás
+    data.valorAceleracionDelante = 0.0;
+    data.valorAceleracionDetras = ((462 - valorAcelerador) * 2.216)/4;
+  }else if(valorAcelerador > 562){
+    //Acelerador en modo marcha delante
+    data.valorAceleracionDetras = 0.0;
+    data.valorAceleracionDelante = ((valorAcelerador - 562) * 2.216)/4;
+  }else if(valorAcelerador > 462 && valorAcelerador <562){
+    //Punto parado
+    data.valorAceleracionDelante = 0.0;
+    data.valorAceleracionDetras = 0.0;
+  }
+  }
+
+  
   radio.write(&data, sizeof(data));
-  delay(1000);
+  delay(50);
 }
--- receiver/Receptorv3/Receptorv3.ino
+++ receiver/Receptorv3/Receptorv3.ino
@@ -12,9 +12,21 @@
 RF24 radio(9, 10); // CE, CSN
 const byte address[6] = "00001";
 
-float data[3];
+struct package
+{
+  float valorAceleracionDelante  = 0.0;
+  float valorAceleracionDetras = 0.0;
+  int  direccion = 0;
+};
+typedef struct package Package;
+Package data;
+
 
 void setup() {
+
+    Serial.begin(9600);
+    Serial.println("Inicializando pines de motores");
+    delay(400);
   //Motor izquierda
   //IN1
    pinMode(6, OUTPUT);
@@ -29,55 +41,69 @@ void setup() {
    pinMode(7, OUTPUT);
   //ENABLE
    pinMode(8,OUTPUT);
-
+ 
    digitalWrite(8, HIGH);
    digitalWrite(2, HIGH);
   
-  Serial.begin(9600);
+  Serial.println("Inicializando radio");
+  
   radio.begin();
   radio.openReadingPipe(0, address);
   radio.setPALevel(RF24_PA_MIN);
   radio.startListening();
+
+  Serial.println("Comunication address: 00001");
+  delay(400);
+  Serial.println("Power Leveol: " + RF24_PA_MIN);
+  delay(400);
+  Serial.println("Bienvendio");
+  delay(200);
+  Serial.println("Sistema inicializado");
+  
 }
 void loop() {
-   int valor;
-   if (radio.available())
-   {    
-      radio.read(data, sizeof data);
-      Serial.print(data[0]);
-      if(data[0] = 1){
-        valor = 1;
-      }else if(data[0] = 2){
-        valor = 2;
-      }else if(data[0] = 3){
-        valor = 3;
-      }else if(data[0] = 4){
-        valor = 4;
-      }
-   }
 
-
-    if(valor = 1){
-      //Hacia delante
-      digitalWrite(6, HIGH);
-      digitalWrite(5, LOW);
-      digitalWrite(11, HIGH);
-      digitalWrite(3, LOW);
-    }else if(valor = 2){
-      //Hacia atrás
-      digitalWrite(6, LOW);
-      digitalWrite(5, HIGH);
-      digitalWrite(11, LOW);
-      digitalWrite(3, HIGH);
-    }else if(valor = 3){
-      //Gira izquierda
-      digitalWrite(11, HIGH);
-      digitalWrite(3, LOW);
-    }else if(valor = 4){
-      //Gira derecha
-      digitalWrite(6, HIGH);
-      digitalWrite(5, LOW);
+   if (radio.available()) 
+  {
+    while (radio.available())
+    {
+      radio.read( &data, sizeof(data) );
     }
-   
-   delay(1000);
+    
+    Serial.print("Direccion");
+    Serial.print(data.direccion);  
+     
+    
+         
+    if(data.direccion == 2){
+      digitalWrite(3, LOW);
+      digitalWrite(5, HIGH);
+      digitalWrite(6, HIGH);
+      digitalWrite(11, LOW);
+    }else if(data.direccion == 4){
+      digitalWrite(3, HIGH);
+      digitalWrite(5, LOW);
+      digitalWrite(6, LOW);
+      digitalWrite(11, HIGH);
+    }else if(data.direccion == 1){
+      if(data.valorAceleracionDelante > 30){
+        analogWrite(3, data.valorAceleracionDelante-30);
+        digitalWrite(5, LOW);
+        analogWrite(6, data.valorAceleracionDelante);
+        digitalWrite(11, LOW);
+      }else if(data.valorAceleracionDetras > 0){
+        digitalWrite(3, LOW);
+        analogWrite(5, data.valorAceleracionDetras);
+        digitalWrite(6, LOW);
+        analogWrite(11, data.valorAceleracionDetras);
+      }else{
+        digitalWrite(3, LOW);
+        digitalWrite(5, LOW);
+        digitalWrite(6, LOW);
+        digitalWrite(11, LOW);
+      }
+    }
+    
+   delay(50);
+  }
 }
